5010137230fadda8de4c94b1a826097abbaba00f
YUJIN <127924591+22112030YUJIN@users.noreply.github.com>
2025-09-14 16:40:56 +0900

    채팅 http,https 둘 다 지원하도록

--- routine-it/src/pages/Group/GroupChat/GroupChatScreen.tsx
+++ routine-it/src/pages/Group/GroupChat/GroupChatScreen.tsx
@@ -19,7 +19,10 @@ import { requestAuthApproval, getGroupMembers } from '../../../api/group';
 import { getUserProfile } from '../../../api/user';
 
 export const BASE_URL = "http://15.164.98.221:8080";
-const WS_CONNECTION_URL = `${BASE_URL}/ws`;
+//const WS_CONNECTION_URL = `${BASE_URL}/ws`;
+const currentProtocol = window.location.protocol;
+const WS_CONNECTION_URL = `${currentProtocol}//${window.location.host}/ws`;
+
 
 export interface Message {
     id: number | null;
